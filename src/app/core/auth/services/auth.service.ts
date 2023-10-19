7cfe60d68416f6a19ff02da2d4b5811affcf75f1
Lisi Tomelin <lisa_tomelin@hotmail.com>
2023-10-19 15:18:32 -0300

    add: behaviour-subject de usuario

--- src/app/core/auth/services/auth.service.ts
+++ src/app/core/auth/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, catchError, map, tap, throwError } from "rxjs";
+import { BehaviorSubject, Observable, catchError, map, tap, throwError } from "rxjs";
 import { RegistrarUsuarioViewModel } from "../models/registrar-usuario.view-model";
 import { TokenViewModel } from "../models/token.view-model";
 import { LocalStorageService } from "src/app/core/auth/services/local-storage.service";
 import { AutenticarUsuarioViewModel } from "../models/autenticar-usuario.view-model copy";
+import { UsuarioTokenViewModel } from "../models/usuario-token.view-model";
 
 @Injectable()
 export class AuthService{
@@ -15,17 +16,38 @@ export class AuthService{
   private endpointRegistrar: string = this.endpoint + 'registrar';
   private endpointLogin: string = this.endpoint + 'autenticar';
 
-  constructor(private http: HttpClient, private LocalStorage: LocalStorageService){}
+  private usuarioAutenticado: BehaviorSubject<UsuarioTokenViewModel | undefined>;
+
+  constructor(private http: HttpClient, private LocalStorage: LocalStorageService)
+  {
+    this.usuarioAutenticado = new BehaviorSubject<
+    UsuarioTokenViewModel | undefined>(undefined);
+  }
+
+  public obterUsuarioAutenticado(){
+    return this.usuarioAutenticado.asObservable();
+  }
 
   public registrar(usuario: RegistrarUsuarioViewModel): Observable<TokenViewModel> {
     return this.http.post<any>(this.endpointRegistrar, usuario).pipe(
       map(res => res.dados), //mapeia a res completa e retorna só os dados
 
       tap((dados: TokenViewModel) => this.LocalStorage.salvarDadosLocalUsuario(dados)),// obter o retorno do map e salva no storage
+
+      tap((dados: TokenViewModel) => this.notificarLogin(dados.usuarioToken)),// notifica os componentes que o usuário realizou o login
       
       catchError((err) => this.processarErroHttp(err))
     );
   }
+
+  private notificarLogin(usuario: UsuarioTokenViewModel): void {
+    this.usuarioAutenticado.next(usuario);
+  }
+
+  public notificarLogout(): void {
+    this.usuarioAutenticado.next(undefined);
+  }
+
 
   public login(usuario: AutenticarUsuarioViewModel): Observable<TokenViewModel> {
     return this.http.post<any>(this.endpointLogin, usuario).pipe(
--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UsuarioTokenViewModel } from 'src/app/core/auth/models/usuario-token.view-model';
+import { AuthService } from 'src/app/core/auth/services/auth.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+
+  usuario$?: Observable<UsuarioTokenViewModel | undefined>;
+
+  constructor(private authService: AuthService){}
+
+
+  ngOnInit(): void {
+    this.usuario$ = this.authService.obterUsuarioAutenticado();
+    
+    
+  }
 
 }
